069763d1caf01688a1a115183141738e8f14869f
David Djukic <davidfdgh@gmail.com>
2018-02-25 23:12:15 +0100

    Add files via upload

--- js/MainController.js
+++ js/MainController.js
@@ -1,3 +1,47 @@
-app.controller('MainController', ['$scope', function($scope){
+app.controller('MainController', ['$scope', 
+function($scope){
+	$scope.promo = 'Our products';
+	$scope.title = 'Die beste kamionen'; 
+	$scope.products = [
+	{
+		name: 'Danish Apple Pastry',
+		price: 10$,
+		image: 'media/images/danish-aeblestang.png'		
+	},
+	{
+		name: 'Cream Danish Pastry',
+		price: 11$,
+		image: 'media/images/danish-ceam-buns.png'
+	},
+	{
+		name:
+		price:
+		image:
+	},
+	{
+		name:
+		price:
+		image:
+	},
+	{
+		name:
+		price:
+		image:
+	},
+	{
+		name:
+		price:
+		image:
+	},
+	{
+		name:
+		price:
+		image:
+	},
 	
+	];
+	/** Search box ***/
+	$scope.search= function(val) {
+		// fetch data
+	};
 }]);
---
+++ js/appDirectives.js
@@ -0,0 +1,10 @@
+app.directive('search', function () {
+    return function ($scope, element) {
+        element.bind("keyup", function (event) {
+          var val = element.val();
+          if(val.length > 2) {
+            $scope.search(val);
+          }
+        });
+    };
+});
