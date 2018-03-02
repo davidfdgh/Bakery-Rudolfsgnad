52464d284910f6c026bce737929d20fcc86ddf4d
David Djukic <davidfdgh@gmail.com>
2018-03-02 04:20:25 +0100

    Add files via upload
    Corrected Angular.js code, solved some issues around Controller

--- js/MainController.js
+++ js/MainController.js
@@ -1,47 +1,52 @@
-app.controller('MainController', ['$scope', 
-function($scope){
+app.controller('MainController',['$scope', function($scope){
 	$scope.promo = 'Our products';
-	$scope.title = 'Die beste kamionen'; 
+	$scope.title = 'Search results...'; 
+	
 	$scope.products = [
-	{
-		name: 'Danish Apple Pastry',
-		price: 10$,
-		image: 'media/images/danish-aeblestang.png'		
+	{   name: 'Danish Apple Pastry',
+		price: '10$',
+		src: 'media/images/danish1.png'		
 	},
 	{
 		name: 'Cream Danish Pastry',
-		price: 11$,
-		image: 'media/images/danish-ceam-buns.png'
+		price: '11$',
+		src: 'media/images/danish2.png'
 	},
 	{
-		name:
-		price:
-		image:
+		name: 'Danish Forest Fruits',
+		price: '7.8$',
+		src: 'media/images/danish3.png'
 	},
 	{
-		name:
-		price:
-		image:
+		name: 'Danish Snails',
+		price: '5$',
+		src: 'media/images/danish4.png'
 	},
 	{
-		name:
-		price:
-		image:
+		name: 'Danish Pastry Triangle',
+		price: '17$',
+		src: 'media/images/danish5.png'
 	},
 	{
-		name:
-		price:
-		image:
+		name: 'Danish Pinwheel',
+		price: '7$',
+		src: 'media/images/danish6.png'
 	},
 	{
-		name:
-		price:
-		image:
-	},
+		name: 'Danish Wienerstang',
+		price: '9$',
+		src: 'media/images/danish7.png'
+	}
+
+
+
+
+
+
 	
 	];
-	/** Search box ***/
+	/** Search box 
 	$scope.search= function(val) {
 		// fetch data
-	};
+	};**/
 }]);
