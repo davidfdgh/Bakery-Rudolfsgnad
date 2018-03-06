2d3fd03c27df6d4a60c339033746589c66f7f45b
David Djukic <davidfdgh@gmail.com>
2018-03-06 22:21:32 +0100

    Add files via upload
    Added to Main Controller, new commands for filters and products

--- js/MainController.js
+++ js/MainController.js
@@ -36,9 +36,52 @@ app.controller('MainController',['$scope', function($scope){
 		name: 'Danish Wienerstang',
 		price: '9$',
 		src: 'media/images/danish7.png'
-	}
-
-
+	},
+	{
+		name: 'Apple Strudel',
+		price: '9.6$',
+		src: 'media/images/applestrudel.png'
+	},
+	{
+		name: 'Croissant Apricot',
+		price: '7.6$',
+		src: 'media/images/croissantapricot.png'
+	},
+	{
+		name: 'Croissant Butter',
+		price: '6$',
+		src: 'media/images/croissantbutter.png'
+	},
+	{
+		name: 'Croissant Chocolate',
+		price: '8$',
+		src: 'media/images/croissantchoco.png'
+	},
+	{
+		name: 'Croissant and Moka Coffee',
+		price: '2$ + 5.6$',
+		src: 'media/images/croissantcoffee.png'
+	},
+	{
+		name: 'Croissant Vanilla',
+		price: '11$',
+		src: 'media/images/croissantvanilla.png'
+	},
+	{
+		name: 'Pain aux Raisins',
+		price: '7$',
+		src: 'media/images/painauxraisins.png'
+	},
+	{
+		name: 'Vanilla & Almond Tart',
+		price: '12$',
+		src: 'media/images/vanillaalmondtart.png'
+	},
+	{
+		name: 'Strawberry Custard Tart',
+		price: '10$',
+		src: 'media/images/strawberryCustardTarts.png'
+	},
 
 
 
